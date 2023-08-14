4fe9aa0e4d34a1fc4779c49a3f33ef5c8c1ec55c
Biswajit Mandal <itsbabin2004@gmail.com>
2023-08-14 20:19:49 +0530

    just cheak

--- sort.cpp
+++ sort.cpp
@@ -27,9 +27,9 @@ void insertion_sort (int arr[] , int n){
     for (int i = 1; i <= n-1; i++)
     {   key = arr[i];
         j = i-1;
-      while (arr[j]> key && j >= 0)
+      while (arr[j] > key && j >= 0)
       {
-        arr[j+1] = arr[j];
+        arr[j+1] = arr[j];  
         j--;
       }
       arr[j+1] = key;
