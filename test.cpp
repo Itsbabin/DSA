3c3af0dfe129516ebc3cc19cbca4db0d27557f96
Biswajit Mandal <itsbabin2004@gmail.com>
2024-01-04 21:19:19 +0530

    new code test

--- test.cpp
+++ test.cpp
@@ -1,17 +1,38 @@
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
+int main()
+{
+  long long int a[] = {-4 , - 2 , 1 , -3};
+  long long int n = 4;
+  long long int k = 2;
 
- int main() {
+  long long maxans = INT_MIN;
+  
+  for (long long int f = 0; f < n; f++)
+  {
+    long long int ans = 0;
+   long long int tempSum = 0;
 
-    string name = "11011" ;
+  for (long long int i = 0; i < k; i++)
+  {
+    tempSum += a[i];
+  }
 
-   //  string new_name = name.substr(4);
+  ans = tempSum;
 
+  for (long long int i = 1; (k + i -1) < n; i++)
+  {
+    long long int fst = i -1;
+    long long int snd = i +k -1;
+    tempSum = tempSum - a[fst] + a[snd];
 
+    ans = max(tempSum, ans);
+  }
+    maxans = max(ans , maxans);
+  }
 
-    cout << decimalNumber << endl;
+     cout  << maxans  << endl;
 
-
-   return 0 ;
+  return 0;
 }
